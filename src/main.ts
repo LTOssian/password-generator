b7434e795ce9c09c5f561e28f35ffeaadcf2b298
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-02-04 18:56:43 +0100

    feat: added header, strengh img and main password cta

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<h1>Hello Louisan</h1>
+<div id="header">
+    <h1>Password Generator</h1>
+    <p>Create strong passwords to keep your account secure online.</p>
+</div>
+<section id="passwordParams">
+    <div class="imgContainer">
+        <img src="../assets/fortress.svg" alt="Image describing the strenght of the password : Fortress"></img>
+    </div>
+    <div class="outputContainer">
+        <div class="passwordOutput">
+            <p class="password">PTx1f5DaFX</p>
+            <span class="strenghBadge">Very Strong</span>
+            <button class="resetButton">
+                <img src="../assets/refresh.svg"></img>
+            </button>
+        </div>
+    </div>
+    <button class="copyButton">Copy</button>
+</section>
+
+
+
 `
